68b105cc4cea1bce6043bf3f7218b2d2a949680f
Yuu-NiKorn <yuuyunikorn@gmail.com>
2025-05-26 18:48:06 +0200

    MIAAAAAAAAAAAAAAAAAW

---
+++ Assets/choix_fin.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceTrigger : MonoBehaviour
+{
+    public GameObject mainChoiceCanvas;     // Le canvas avec les 2 boutons
+    public GameObject canvasOption1;        // Canvas pour choix 1
+    public GameObject canvasOption2;        // Canvas pour choix 2
+
+    private bool hasTriggered = false;
+
+    private void Start()
+    {
+        mainChoiceCanvas.SetActive(false);
+        canvasOption1.SetActive(false);
+        canvasOption2.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasTriggered && other.CompareTag("Player"))
+        {
+            hasTriggered = true;
+            //Time.timeScale = 0f;
+            mainChoiceCanvas.SetActive(true);
+            Debug.Log("le joueur est entré dans la zone");
+        }
+    }
+
+    public void OnChoice1()
+    {
+        mainChoiceCanvas.SetActive(false);
+        canvasOption1.SetActive(true);
+    }
+
+    public void OnChoice2()
+    {
+        mainChoiceCanvas.SetActive(false);
+        canvasOption2.SetActive(true);
+    }
+}
